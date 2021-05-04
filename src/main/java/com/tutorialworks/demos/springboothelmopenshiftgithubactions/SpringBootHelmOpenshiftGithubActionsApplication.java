4eddcd1e16d9e528a037bf88e5f3125053c05f38
Tom Donohue <monodot@gmail.com>
2021-05-04 14:48:45 +0100

    Initial commit

---
+++ src/main/java/com/tutorialworks/demos/springboothelmopenshiftgithubactions/SpringBootHelmOpenshiftGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialworks.demos.springboothelmopenshiftgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelmOpenshiftGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelmOpenshiftGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorialworks/demos/springboothelmopenshiftgithubactions/SpringBootHelmOpenshiftGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialworks.demos.springboothelmopenshiftgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelmOpenshiftGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
